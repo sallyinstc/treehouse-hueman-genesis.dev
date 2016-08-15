69ab9cab75117ca277c2fbc3e3ae95c141889a29
Sally <sallyinstc@gmail.com>
2016-08-15 15:34:53 -0500

    Update of nav styles

--- wp-content/themes/treehouse-hueman-genesis/front-page.php
+++ wp-content/themes/treehouse-hueman-genesis/front-page.php
@@ -14,23 +14,23 @@ add_action( 'genesis_meta', 'hueman_home_genesis_meta' );
  */
 function hueman_home_genesis_meta() {
 
-	if ( is_active_sidebar( 'home-top' ) || is_active_sidebar( 'home-bottom' ) ) {
+	if ( is_active_sidebar( 'home-top' ) || is_active_sidebar( 'home-bottom' )) {
 
-		//* Force sidebar-content-sidebar layout
+		//* Force sidebar-content-sidebar layout setting
 		add_filter( 'genesis_site_layout', '__genesis_return_sidebar_content_sidebar' );
 
 		//* Add hueman-pro-home body class
-		//add_filter( 'body_class', 'hueman_body_class' );
-		
+		add_filter( 'body_class', 'hueman_body_class' );
+
 		//* Remove breadcrumbs
 		remove_action( 'genesis_before_loop', 'genesis_do_breadcrumbs' );
 
 		//* Remove the default Genesis loop
 		//remove_action( 'genesis_loop', 'genesis_do_loop' );
-		
-				//* Add home widgets
-		add_action( 'genesis_loop', 'hueman_home_widgets');
-		
+
+		//* Add home widgets
+		add_action( 'genesis_loop', 'hueman_home_widgets' );
+
 	}
 }
 
@@ -42,13 +42,14 @@ function hueman_home_widgets() {
 		'before' => '<div class="home-top widget-area">',
 		'after'  => '</div>',
 	) );
-	
+
 	genesis_widget_area( 'home-bottom', array(
 		'before' => '<div class="home-bottom widget-area">',
 		'after'  => '</div>',
 	) );
-	
+
 	echo '</div>';
+
 }
 
 genesis();
--- wp-content/themes/treehouse-hueman-genesis/functions.php
+++ wp-content/themes/treehouse-hueman-genesis/functions.php
@@ -14,38 +14,57 @@ add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list'
 add_theme_support( 'genesis-responsive-viewport' );
 
 //* Enqueue scripts and styles
-add_action('wp_encueue_scripts', 'hueman_scripts_styles');
-function hueman_scripts_styles (){
-    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awsome/4.3.0/css/font-awesome.min.css', array(), 'treehouse-hueman-genesis');
+add_action( 'wp_enqueue_scripts', 'hueman_scripts_styles' );
+function hueman_scripts_styles() {
+	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css', array(), 'treehouse-hueman-genesis' );
 }
 
 //* Add support for structural wraps
-add_theme_support('genesis-structural-wraps', array(
-    'header', 'nav', 'subnav', 'main', 'footer-widgets', 'footer',
-));
+add_theme_support( 'genesis-structural-wraps', array(
+	'header', 'nav', 'subnav', 'main', 'footer-widgets', 'footer',
+) );
 
 //* Rename menus
-add_theme_support('genesis-menus', array(
-    'primary' => __('Header Top Navigation Menu', 'treehouse-hueman-genesis'),
-    'secondary' => __('Header Bottom Navigation Menu', 'treehouse-hueman-genesis')
-));
+add_theme_support( 'genesis-menus', array(
+	'primary' => __( 'Header Top Navigation Menu', 'treehouse-hueman-genesis' ),
+	'secondary' => __( 'Header Bottom Navigation Menu', 'treehouse-hueman-genesis' )
+) );
 
-//*Add new image sizes
-add_image_size('home-top', 780, 354, TRUE);
-add_image_size('home-middle', 375, 175, TRUE);
+//* Relocate Primary (top) Navigation
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_before', 'genesis_do_nav', 4 );
+
+//* Relocate Header outside Site Container
+remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
+remove_action( 'genesis_header', 'genesis_do_header' );
+remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
+add_action( 'genesis_before', 'genesis_header_markup_open', 5 );
+add_action( 'genesis_before', 'genesis_do_header' );
+add_action( 'genesis_before', 'genesis_header_markup_close', 15 );
+
+//* Relocate Secondary (bottom) Navigation
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_before', 'genesis_do_subnav', 14 );
+
+//* Move the Secondary Sidebar into the Content Sidebar Wrap area.
+remove_action( 'genesis_after_content_sidebar_wrap', 'genesis_get_sidebar_alt' );
+add_action( 'genesis_after_content', 'genesis_get_sidebar_alt' );
+
+//* Add new image sizes
+add_image_size( 'home-top', 780, 354, TRUE );
+add_image_size( 'home-middle', 375, 175, TRUE );
 
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
 
 //* Register widget areas
-genesis_register_sidebar(array(
-    'id'            => 'home-top',
-    'name'          => __('Home Top', 'trehouse-hueman-genesis'),
-    'description'   => __('Widgets in this section display in the top widget area on the homepage.', 'treehouse-hueman-genesis'),
-));
-
-genesis_register_sidebar(array(
-    'id'            => 'home-bottom',
-    'name'          => __('Home Bottom', 'trehouse-hueman-genesis'),
-    'description'   => __('Widgets in this section display in the bottom widget area on the homepage.', 'treehouse-hueman-genesis'),
-));
+genesis_register_sidebar( array(
+	'id'          => 'home-top',
+	'name'        => __( 'Home Top', 'treehouse-hueman-genesis' ),
+	'description' => __( 'Widgets in this section will display in the top widget area on the homepage.', 'treehouse-hueman-genesis' ),
+) );
+genesis_register_sidebar( array(
+	'id'          => 'home-bottom',
+	'name'        => __( 'Home Bottom', 'treehouse-hueman-genesis' ),
+	'description' => __( 'Widgets in this section will display in the bottom widget area on the homepage.', 'treehouse-hueman-genesis' ),
+) );
